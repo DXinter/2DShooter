2e96ddd14d7a11a572776f716b1c6f440b0019e8
Даниил Хинтер <vinny.banan@gmail.com>
2024-12-10 15:58:03 +0300

    Добавил сцену

--- Assets/Scripts/Game/Player/PlayerFacade.cs
+++ Assets/Scripts/Game/Player/PlayerFacade.cs
@@ -7,8 +7,6 @@ namespace Game.Player
     public class PlayerFacade : Dependency, ITickable
     {
         [SerializeField] private GameObject characterAvatar;
-
-        private Transform _transform;
         
         [Inject]
         public void Construct()
